36cae17759243f4a0112b5129f2f50e7e99450d9
Marcelo Junior <marcelo.jr63@gmail.com>
2025-06-13 08:00:21 -0300

    Export to wasm

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use wasm_bindgen::prelude::*;
+
+mod ast2pdft;
+mod parser;
+mod pdf_tree;
+
+#[wasm_bindgen]
+pub fn compile_pdfl(input: &str) -> Result<Box<[u8]>, JsValue> {
+    let ast = parser::parse(input).map_err(|e| JsValue::from_str(&e))?;
+    let pdft = ast2pdft::to_pdft(ast);
+    Ok(pdft.to_buffer().into_boxed_slice())
+}
